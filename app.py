12f5c1518a4c9f2411af0a03a6e100107e4281db
Amit Raina <write2amitr@yahoo.com>
2025-09-27 09:22:23 +0530

    Added backend route

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, jsonify
 import json
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+MONGO_URI = "mongodb+srv://<username>:<password>@<cluster-url>/todo_db?retryWrites=true&w=majority"
+client = MongoClient(MONGO_URI)
+
+# Database and collection
+db = client["todo_db"]
+todos_collection = db["todos"]
+
+
 
 @app.route('/api', methods=['GET'])
 def get_data():
@@ -11,6 +21,23 @@ def get_data():
         return jsonify(data), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo_item():
+   try:
+       data = request.get_json()  # Expecting JSON input
+       item_name = data.get("itemName")
+       item_description = data.get("itemDescription")
+       if not item_name:
+           return jsonify({"error": "Item Name is required"}), 400
+       todo = {
+           "itemName": item_name,
+           "itemDescription": item_description
+       }
+       todos_collection.insert_one(todo)
+       return jsonify({"message": "To-Do item added successfully", "todo": todo}), 201
+   except Exception as e:
+       return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
